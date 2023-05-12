0e0d38477998ee371f21b5dfbedad764960a5908
Renan Cavichi <renancavichi@gmail.com>
2023-05-11 21:39:16 -0300

    create fetch api stocks

--- js/main.js
+++ js/main.js
@@ -14,5 +14,22 @@ const handleModalClose = (event) => {
     }
 }
 
+const handleAddTicker = async (event) => {
+    event.preventDefault() 
+    const ticker = event.target.ticker.value
+    try{
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=XXXXXXXXX`)
+        const data = await response.json()
+        console.log(data)
+        if(data["Global Quote"]["05. price"]){
+            alert('Deu certo!')
+        }else{
+            alert(`Ticker ${ticker} não encontrado!`)
+        }
+    } catch(error){
+        alert(error)
+    }
+}
+
 const modal = document.querySelector(".modal")
 modal.addEventListener("click", handleModalClose)
