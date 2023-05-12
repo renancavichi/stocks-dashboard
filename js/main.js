10ecf6038e93a32923fcd895338a1f0c92adc104
Renan Cavichi <renancavichi@gmail.com>
2023-05-11 22:18:25 -0300

    add ticker to list

--- js/main.js
+++ js/main.js
@@ -15,14 +15,23 @@ const handleModalClose = (event) => {
 }
 
 const handleAddTicker = async (event) => {
-    event.preventDefault() 
-    const ticker = event.target.ticker.value
+    event.preventDefault() // impede que o form seja enviado
+    const ticker = event.target.ticker.value // pega o valor do input ticker
     try{
-        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=XXXXXXXXX`)
-        const data = await response.json()
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=XXXXXXXXX`) // faz a requisição na API
+        const data = await response.json() // transforma a resposta JSON em objeto
         console.log(data)
+        const price = data["Global Quote"]["05. price"]
         if(data["Global Quote"]["05. price"]){
-            alert('Deu certo!')
+            const newTicker = 
+            `<div class="ticker">
+                <h2>${ticker}</h2>
+                <p>${price}</p>
+            </div>
+            `
+            const tickersList = document.querySelector("#tickers-list")
+            tickersList.innerHTML += newTicker
+            closeModal('#add-stock')
         }else{
             alert(`Ticker ${ticker} não encontrado!`)
         }
