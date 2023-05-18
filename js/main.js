1e2fe979e2059ead8c20ebae5361155ccc8d40d6
Renan Cavichi <renancavichi@gmail.com>
2023-05-18 20:44:44 -0300

    add ticker price change

--- js/main.js
+++ js/main.js
@@ -18,27 +18,59 @@ const handleAddTicker = async (event) => {
     event.preventDefault() // impede que o form seja enviado
     const ticker = event.target.ticker.value // pega o valor do input ticker
     try{
-        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=XXXXXXXXX`) // faz a requisição na API
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=HAUCFV4MBFM2A2Z1`) // faz a requisição na API
         const data = await response.json() // transforma a resposta JSON em objeto
-        console.log(data)
         const price = data["Global Quote"]["05. price"]
-        if(data["Global Quote"]["05. price"]){
+        const previousClosePrice = data["Global Quote"]["08. previous close"]
+        if(price && previousClosePrice){
+            const priceFormatted = parseFloat(price).toFixed(2)
+            const previousClosePriceFormatted = parseFloat(previousClosePrice).toFixed(2)
+            let priceChange = ''
+            let Symbol = ''
+            if(priceFormatted !== previousClosePriceFormatted){
+                if(priceFormatted > previousClosePriceFormatted){
+                    priceChange = 'increase'
+                    Symbol = '▲'
+                }else{
+                    priceChange = 'decrease'
+                    Symbol = '▼'
+                }
+            }
+
             const newTicker = 
             `<div class="ticker">
                 <h2>${ticker}</h2>
-                <p>${price}</p>
+                <p class="${priceChange}">${Symbol} U$ ${priceFormatted}</p>
             </div>
             `
             const tickersList = document.querySelector("#tickers-list")
-            tickersList.innerHTML += newTicker
+            tickersList.innerHTML = newTicker + tickersList.innerHTML
             closeModal('#add-stock')
         }else{
             alert(`Ticker ${ticker} não encontrado!`)
         }
-    } catch(error){
+    } catch (error){
         alert(error)
     }
 }
 
+const handleTickerMouseEnter = (event) => {
+    const ticker = event.target
+    const btnClose = ticker.querySelector(".btn-close")
+    btnClose.style.display = "block"
+}
+
+const handleTickerMouseLeave = (event) => {
+    const ticker = event.target
+    const btnClose = ticker.querySelector(".btn-close")
+    btnClose.style.display = "none"
+}
+
 const modal = document.querySelector(".modal")
+const tickers = document.querySelectorAll(".ticker")
 modal.addEventListener("click", handleModalClose)
+
+tickers.forEach((ticker) => {
+    ticker.addEventListener("mouseenter", handleTickerMouseEnter)
+    ticker.addEventListener("mouseleave", handleTickerMouseLeave)
+})
