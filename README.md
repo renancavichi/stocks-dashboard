b7b010c22cd2f02644ad3863f65d58783a4f55a7
Renan Cavichi <renancavichi@gmail.com>
2023-05-25 22:26:55 -0300

    refresh ticker

---
+++ README.md
@@ -0,0 +1,7 @@
+# Atividades do Projeto
+
+#### 1) Adicionar um botão de atualizar no ticker que traz o valor atualizado
+
+
+#### 2) Avisar o usuário se um ticker já foi adicionado no dashboard para não adicinar novamente por engano.
+
--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ const handleAddTicker = async (event) => {
             `
             const tickersList = document.querySelector("#tickers-list")
             tickersList.innerHTML = newTicker + tickersList.innerHTML
-            addTickersCloseEvents()
+            addTickersEvents()
             closeModal('#add-stock')
         }else{
             alert(`Ticker ${ticker} não encontrado!`)
@@ -56,13 +56,44 @@ const handleAddTicker = async (event) => {
     }
 }
 
+const refreshTicker = async (event) => {
+    const divTicker = event.target.closest('.ticker')
+    const ticker = divTicker.querySelector('h2').textContent
+    const pPrice = divTicker.querySelector('p')
+    const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=HAUCFV4MBFM2A2Z1`) // faz a requisição na API
+    const data = await response.json() // transforma a resposta JSON em objeto
+    const price = data["Global Quote"]["05. price"]
+    const previousClosePrice = data["Global Quote"]["08. previous close"]
+    if(price && previousClosePrice){
+        const priceFormatted = parseFloat(price).toFixed(2)
+        const previousClosePriceFormatted = parseFloat(previousClosePrice).toFixed(2)
+        let priceChange = ''
+        let Symbol = ''
+        if(priceFormatted !== previousClosePriceFormatted){
+            if(priceFormatted > previousClosePriceFormatted){
+                priceChange = 'increase'
+                Symbol = '▲'
+            }else{
+                priceChange = 'decrease'
+                Symbol = '▼'
+            }
+        }
+        pPrice.innerHTML = `${Symbol} ${priceFormatted}`
+        pPrice.className = priceChange
+    }else{
+        alert(`Ticker ${ticker} não encontrado para atualização!`)
+    }
+}
+
 const handleTickerMouseEnter = (event) => {
     const ticker = event.target
     const btnClose = ticker.querySelector(".btn-close")
+    const btnRefresh = ticker.querySelector(".btn-refresh")
     btnClose.style.display = "block"
+    btnRefresh.style.display = "block"
 }
 
-const addTickersCloseEvents = () => {
+const addTickersEvents = () => {
     const tickers = document.querySelectorAll(".ticker")
     tickers.forEach((ticker) => {
         ticker.addEventListener("mouseenter", handleTickerMouseEnter)
@@ -73,7 +104,9 @@ const addTickersCloseEvents = () => {
 const handleTickerMouseLeave = (event) => {
     const ticker = event.target
     const btnClose = ticker.querySelector(".btn-close")
+    const btnRefresh = ticker.querySelector(".btn-refresh")
     btnClose.style.display = "none"
+    btnRefresh.style.display = "none"
 }
 
 const removeTicker = (event) => {
@@ -85,4 +118,4 @@ const removeTicker = (event) => {
 const modal = document.querySelector(".modal")
 modal.addEventListener("click", handleModalClose)
 
-addTickersCloseEvents()
+addTickersEvents()
